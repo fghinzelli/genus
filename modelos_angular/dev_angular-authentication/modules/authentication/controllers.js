c058c4215a1590a928b794711feccdad2f038a11
Fernando Ghinzelli <fghinzelli@gmail.com>
2017-10-27 00:40:48 -0200

    Reestruturacao para exibir dados com Angular JS

---
+++ modelos_angular/dev_angular-authentication/modules/authentication/controllers.js
@@ -0,0 +1,23 @@
+'use strict';
+ 
+angular.module('Authentication')
+ 
+.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function ($scope, $rootScope, $location, AuthenticationService) {
+        // reset login status
+        AuthenticationService.ClearCredentials();
+ 
+        $scope.login = function () {
+            $scope.dataLoading = true;
+            AuthenticationService.Login($scope.username, $scope.password, function(response) {
+                if(!response.error) {
+                    AuthenticationService.SetCredentials($scope.username, response.token);
+                    $location.path('/');
+                } else {
+                    $scope.error = response.error['message'];
+                    $scope.dataLoading = false;
+                }
+            });
+        };
+    }]);
