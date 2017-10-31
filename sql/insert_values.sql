4d49bd4c49dc28fc66fde9261bf1bb44e685895c
Fernando Ghinzelli <fghinzelli@gmail.com>
2017-10-31 15:32:36 -0200

    Atualizacao de scripts de dados iniciais

--- sql/insert_values.sql
+++ sql/insert_values.sql
@@ -13,8 +13,15 @@ INSERT INTO `AcessoNivel` (`id`, `descricao`, `status`) VALUES (NULL, 'Administr
 -- Pessoa
 INSERT INTO `Pessoa` (`id`, `nome`, `sexo`, `nomePai`, `nomeMae`, `dataNascimento`, `telefone1`, `telefone2`, `cpf`, `rg`, `email`, `logradouro`, `numero`, `complemento`, `bairro`, `municipioId`, `numeroDizimo`, `comunidadeId`, `observacoes`, `batizado`, `localBatismo`, `primeiraEucaristia`, `localPrimeiraEucaristia`, `status`, `dataUltimaAlteracao`, `usuarioUltimaAlteracaoId`) 
 VALUES 
-(NULL, 'Fernando Ghinzelli', 'M', 'Jose Luis Ghinzelli', 'Lourdes Ghinzelli', '2017-08-14 00:00:00', NULL, NULL, NULL, NULL, 'fghinzelli@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
-(NULL, 'Mariel Carissimi', 'F', 'Mario Carissimi', 'Teresa Maria Pasa Carissimi', '2017-06-11 00:00:00', NULL, NULL, NULL, NULL, 'mariel@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL);
+(NULL, 'Fulano de Tal', 'M', 'Adão de Tal', 'Eva de Tal', '2017-08-14 00:00:00', NULL, NULL, NULL, NULL, 'fghinzelli@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Maria da Silva', 'F', 'Mario da Silva', 'Isabel da Silva', '2017-06-11 00:00:00', NULL, NULL, NULL, NULL, 'mariel@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Antônio de Jesus', 'M', 'João de Jesus', 'Marta de Jesus', '2017-08-14 00:00:00', NULL, NULL, NULL, NULL, 'fghinzelli@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Salete da Costa', 'F', 'Mario da Costa', 'Teresa da Costa', '2017-06-11 00:00:00', NULL, NULL, NULL, NULL, 'mariel@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Rafael Gonçalves', 'M', 'Jose Gonçalves', 'Lourdes Gonçalves', '2017-08-14 00:00:00', NULL, NULL, NULL, NULL, 'fghinzelli@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Simone da Cruz', 'F', 'Mario da Cruz', 'Salete da Cruz', '2017-06-11 00:00:00', NULL, NULL, NULL, NULL, 'mariel@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Gabriel da Costa', 'M', 'Douglas da Costa', 'Maria da Costa', '2017-08-14 00:00:00', NULL, NULL, NULL, NULL, 'fghinzelli@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL),
+(NULL, 'Jéssica Antunes', 'F', 'Mario Antunes', 'Maria Antunes', '2017-06-11 00:00:00', NULL, NULL, NULL, NULL, 'mariel@gmail.com', NULL, NULL, NULL, NULL, NULL, '12345', 1,  NULL, 1, NULL, NULL, NULL, 1, NULL, NULL);
+
 
 -- Usuario
 INSERT INTO `Usuario` (`id`, `username`, `senha`, `pessoaId`, `token`, `tokenExpiracao`, `status`, `dataUltimaAlteracao`, `usuarioUltimaAlteracaoId`) VALUES (NULL, 'fghinzelli', '$2y$10$K/V0l5OwEQH59sZiNLoG4eRWAhcGIgHgDiR2/tVNeWvJRNEGFmwDO', 1, '07d5ccaeb0da5486', '2017-10-31 03:23:13', 1, NULL, NULL);
